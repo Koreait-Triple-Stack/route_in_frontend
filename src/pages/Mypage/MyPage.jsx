34394d733e8bf194788a862120d44ad1cc9bf80c
heewon <heewon@heewonui-MacBookAir.local>
2026-01-12 12:07:00 +0900

    Init

---
+++ src/pages/Mypage/MyPage.jsx
@@ -0,0 +1,9 @@
+/** @jsxImportSource @emotion/react */
+import * as s from "./styles";
+import React from "react";
+
+function MyPage() {
+    return <div css={s.container}>MyPage</div>;
+}
+
+export default MyPage;
