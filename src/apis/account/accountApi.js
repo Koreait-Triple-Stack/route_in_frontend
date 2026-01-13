cbdc37dfcbc21f77bb6c9f7987e27674d7333b72
yoonho0624 <qkrdbsgh0624@gmail.com>
2026-01-13 12:05:57 +0900

    Feat : mypage, mainpage

---
+++ src/apis/account/accountApi.js
@@ -0,0 +1,34 @@
+import { instance } from "../utils/instance";
+
+export const getUserByUserIdRequest = async (userId) => {
+    instance.interceptors.request.use(
+    (config) => {
+        const accessToken = localStorage.getItem("AccessToken");
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+    const response = await instance.get(`/user/account/userId/${userId}`);
+    return response;
+};
+
+export const getPrincipal = async () => {
+    instance.interceptors.request.use((config) => {
+        const accessToken = localStorage.getItem("AccessToken");
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        return config;
+    });
+    try {
+        const response = await instance.get("/user/account/principal");
+        return response;
+    } catch (error) {
+        return error.response;
+    }
+};
