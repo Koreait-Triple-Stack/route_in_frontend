a8f0f02c72c27d8251ca8bf44c7139430fe55150
yoonho0624 <qkrdbsgh0624@gmail.com>
2026-01-15 18:27:33 +0900

    Feat : MainPage

--- src/apis/routine/routineApi.js
+++ src/apis/routine/routineApi.js
@@ -1,6 +1,14 @@
 import { instance } from "../utils/instance";
 
-export const getRoutineByUserIdRequest = async (userId) => {
-    const response = await instance.get(`/routine/get/user/${userId}`);
+export const updateRoutineRequest = async (data) => {
+    const response = await instance.post("/routine/update", data);
+    return response.data;
+};
+
+export const getRoutineRequest = async (userId) => {
+    const response = await instance.post("/routine/get", {
+        userId: userId,
+        boardId: null 
+    });
     return response.data;
 };
