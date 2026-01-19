3cf34f6801f291a33604f9e236870697b440ef79
toma9512 <gmldnjs985@gmail.com>
2026-01-19 16:46:30 +0900

    Fix: boardList

--- src/apis/utils/tagMaps.js
+++
@@ -1,5 +0,0 @@
-import { EXERCISE_TAGS } from "../../constants/exerciseTags";
-
-export const TAG_ID_TO_LABEL = Object.fromEntries(
-    EXERCISE_TAGS.map((tag) => [tag.id, tag.label])
-);
--- src/pages/MapView.jsx
+++ src/pages/MapView.jsx
@@ -3,23 +3,22 @@ import { getCourseFavoriteByUserId } from "../apis/course/courseService";
 import CourseDetail from "../components/Course/CourseDetail";
 import { Box } from "@mui/system";
 import CourseEdit from "../components/Course/CourseEdit";
+import { usePrincipalState } from "../store/usePrincipalState";
 
 export default function MapView() {
-    const userId = 20;
+    const { principal } = usePrincipalState();
     const {
         data: response,
         isLoading,
         error,
     } = useQuery({
-        queryKey: ["getCourseFavoriteByUserId"],
-        queryFn: () => getCourseFavoriteByUserId(userId),
+        queryKey: ["getCourseFavoriteByUserId", principal?.userId],
+        queryFn: () => getCourseFavoriteByUserId(principal.userId),
         staleTime: 30000,
     });
 
     if (isLoading) return <Box>로딩중...</Box>;
     if (error) return <Box>{error.message}</Box>;
-
-    console.log(response)
 
     return <CourseDetail course={response.data} />;
 }
