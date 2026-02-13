9e6fbea0a3ace5a405675c730fc3c55fb05dc014
toma9512 <gmldnjs985@gmail.com>
2026-02-14 00:58:04 +0900

    Fix: chat

--- src/components/ChatRoomLayout.jsx
+++ src/components/ChatRoomLayout.jsx
@@ -4,13 +4,10 @@ export default function ChatRoomLayout({ children }) {
     return (
         <Box
             sx={{
-                height: "100svh",
-                minHeight: "100dvh",
+                height: "100dvh",
                 display: "flex",
                 flexDirection: "column",
                 overflow: "hidden",
-                overscrollBehavior: "none",
-                touchAction: "manipulation",
                 width: "100%",
             }}>
             {children}
