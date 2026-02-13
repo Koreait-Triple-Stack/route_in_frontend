617fff4c0798b2c419cc5362f3f0efef8ee47179
toma9512 <gmldnjs985@gmail.com>
2026-02-13 16:24:44 +0900

    Fix: chat

--- src/components/ChatRoomLayout.jsx
+++ src/components/ChatRoomLayout.jsx
@@ -1,15 +1,17 @@
 import { Box } from "@mui/system";
 
 export default function ChatRoomLayout({ children }) {
-
     return (
         <Box
             sx={{
-                height: "100dvh",
+                height: "100svh",
                 minHeight: "100dvh",
+                display: "flex",
+                flexDirection: "column",
                 overflow: "hidden",
                 overscrollBehavior: "none",
                 touchAction: "manipulation",
+                width: "100%",
             }}>
             {children}
         </Box>
